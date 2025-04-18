151e0af2a6f909707ca74a7efcd4c7e45f2df145
Fatra Syahreza <fatra.syahreza07@gmail.com>
2025-04-18 21:52:45 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController; 
 
-
-
-
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'submit'])->name('login.submit');
+
+Route::get('/test', function () {
+    return view('test');
+});
+
 ?>
