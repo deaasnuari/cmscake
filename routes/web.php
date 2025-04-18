e8323f5411a37f7cbfa452cd538da0ab32b4a07d
Fatra Syahreza <fatra.syahreza07@gmail.com>
2025-04-18 22:06:46 +0700

    upd

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,19 @@ use App\Http\Controllers\LoginController;
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'submit'])->name('login.submit');
+
+Route::get('/test', function () {
+    return view('test');
+});
+
+Route::get('/catalog', function () {
+    return view('catalog');
+});
+
+Route::get('/profil_user', function () {
+    return view('profil_user');
+});
+
+
+
 ?>
